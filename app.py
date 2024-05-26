47ff2e27d1e50794a94b94500ae16d1bc11dbf63
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-05-26 14:14:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
@@ -15,7 +14,7 @@ df_datos_completo = st.session_state['df_datos_completo']
 nombres_columnas = [col for col in df_datos_completo.columns if col != 'Fecha']
 
 # Opción para seleccionar qué columna graficar
-columna_seleccionada = st.sidebar.selectbox("Selecciona la columna para graficar:", nombres_columnas)
+columna_seleccionada = st.selectbox("Selecciona la columna para graficar:", nombres_columnas)
 
 # Opciones de visualización
 opciones_visualizacion = {
@@ -25,7 +24,7 @@ opciones_visualizacion = {
     'Últimos 7 días': df_datos_completo[df_datos_completo['Fecha'] >= datetime.now() - timedelta(days=7)],
     'Día actual': df_datos_completo[df_datos_completo['Fecha'].dt.date == datetime.now().date()]
 }
-opcion_visualizacion = st.sidebar.selectbox("Selecciona la opción de visualización:", list(opciones_visualizacion.keys()))
+opcion_visualizacion = st.selectbox("Selecciona la opción de visualización:", list(opciones_visualizacion.keys()))
 
 # Obtener los datos para la opción seleccionada
 datos_visualizacion = opciones_visualizacion[opcion_visualizacion]
@@ -40,13 +39,13 @@ if opcion_visualizacion == 'Día actual':
     datos_visualizacion = datos_visualizacion.sort_values(by='Fecha')
     # Formatear la fecha para mostrar solo la hora
     datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M')
-    sns.lineplot(x='Hora', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    ax.plot(datos_visualizacion['Hora'], datos_visualizacion[columna_seleccionada])
     ax.set_xlabel('Hora')
     # Ajustar las etiquetas del eje X para mostrar solo cada hora
     x_ticks = datos_visualizacion['Hora'].iloc[::12]  # Cada 12*5 = 60 minutos = 1 hora
     ax.set_xticks(x_ticks)
 else:
-    sns.lineplot(x='Fecha', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    ax.plot(datos_visualizacion['Fecha'], datos_visualizacion[columna_seleccionada])
     ax.set_xlabel('Fecha')
 
 ax.set_ylabel(columna_seleccionada)
@@ -55,10 +54,3 @@ plt.tight_layout()
 
 # Mostrar la gráfica en Streamlit
 st.pyplot(fig)
-
-# Descripción del proyecto
-st.markdown("""
-# Monitor de Mediciones
-Este proyecto muestra la evolución de las mediciones en diferentes períodos de tiempo. 
-Puedes seleccionar la columna de datos que deseas visualizar y el período de tiempo correspondiente.
-""")
