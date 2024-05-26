c58f28913937bc54705b9f258a1abd4946c6e506
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-05-26 15:05:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,18 @@ opcion_visualizacion = st.sidebar.selectbox("Selecciona la opción de visualizac
 # Obtener los datos para la opción seleccionada
 datos_visualizacion = opciones_visualizacion[opcion_visualizacion]
 
+# Crear un diccionario de colores para asociar cada tipo de medición con un color específico
+colores = {
+    'Temperatura': 'red',
+    'Humedad': 'blue',
+    'Intensidad de la luz': 'green',
+    'Presión': 'orange',
+    'Dirección del viento': 'purple',
+    'Velocidad del viento': 'cyan',
+    'Lluvia a la hora': 'brown',
+    'Índice UV': 'magenta'
+}
+
 # Crear la gráfica
 st.title(f'Gráfico de {columna_seleccionada} - {opcion_visualizacion}')
 fig, ax = plt.subplots()
@@ -38,12 +50,14 @@ fig, ax = plt.subplots()
 if opcion_visualizacion == 'Día actual':
     datos_visualizacion = datos_visualizacion.sort_values(by='Fecha')
     datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M')
-    sns.lineplot(x='Hora', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    for col in nombres_columnas:
+        sns.lineplot(x='Hora', y=col, data=datos_visualizacion, ax=ax, color=colores[col], label=col)
     ax.set_xlabel('Hora')
     x_ticks = datos_visualizacion['Hora'].iloc[::12]
     ax.set_xticks(x_ticks)
 else:
-    sns.lineplot(x='Fecha', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    for col in nombres_columnas:
+        sns.lineplot(x='Fecha', y=col, data=datos_visualizacion, ax=ax, color=colores[col], label=col)
     ax.set_xlabel('Fecha')
     ax.xaxis.set_major_locator(plt.MaxNLocator(10))  # Ajustar para mostrar máximo 10 etiquetas
 
@@ -51,6 +65,7 @@ ax.set_ylabel(columna_seleccionada)
 ax.set_title(f'{columna_seleccionada} a lo largo de {opcion_visualizacion}')
 plt.xticks(rotation=45)
 plt.tight_layout()
+plt.legend()  # Agregar leyenda
 
 # Mostrar la gráfica en Streamlit
 st.pyplot(fig)
