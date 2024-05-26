4ebae2de9a8546d6743919bd056a05288caa440a
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-05-26 14:06:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
@@ -14,7 +15,7 @@ df_datos_completo = st.session_state['df_datos_completo']
 nombres_columnas = [col for col in df_datos_completo.columns if col != 'Fecha']
 
 # Opción para seleccionar qué columna graficar
-columna_seleccionada = st.selectbox("Selecciona la columna para graficar:", nombres_columnas)
+columna_seleccionada = st.sidebar.selectbox("Selecciona la columna para graficar:", nombres_columnas)
 
 # Opciones de visualización
 opciones_visualizacion = {
@@ -24,7 +25,7 @@ opciones_visualizacion = {
     'Últimos 7 días': df_datos_completo[df_datos_completo['Fecha'] >= datetime.now() - timedelta(days=7)],
     'Día actual': df_datos_completo[df_datos_completo['Fecha'].dt.date == datetime.now().date()]
 }
-opcion_visualizacion = st.selectbox("Selecciona la opción de visualización:", list(opciones_visualizacion.keys()))
+opcion_visualizacion = st.sidebar.selectbox("Selecciona la opción de visualización:", list(opciones_visualizacion.keys()))
 
 # Obtener los datos para la opción seleccionada
 datos_visualizacion = opciones_visualizacion[opcion_visualizacion]
@@ -35,22 +36,28 @@ fig, ax = plt.subplots()
 
 # Verificar si la opción de visualización es para el día actual
 if opcion_visualizacion == 'Día actual':
-    # Ordenar los datos cronológicamente
     datos_visualizacion = datos_visualizacion.sort_values(by='Fecha')
-    # Formatear la fecha para mostrar solo la hora
     datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M')
-    ax.plot(datos_visualizacion['Hora'], datos_visualizacion[columna_seleccionada])
+    sns.lineplot(x='Hora', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
     ax.set_xlabel('Hora')
-    # Ajustar las etiquetas del eje X para mostrar solo cada hora
-    x_ticks = datos_visualizacion['Hora'].iloc[::12]  # Cada 12*5 = 60 minutos = 1 hora
+    x_ticks = datos_visualizacion['Hora'].iloc[::12]
     ax.set_xticks(x_ticks)
 else:
-    ax.plot(datos_visualizacion['Fecha'], datos_visualizacion[columna_seleccionada])
+    sns.lineplot(x='Fecha', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
     ax.set_xlabel('Fecha')
+    ax.xaxis.set_major_locator(plt.MaxNLocator(10))  # Ajustar para mostrar máximo 10 etiquetas
 
 ax.set_ylabel(columna_seleccionada)
+ax.set_title(f'{columna_seleccionada} a lo largo de {opcion_visualizacion}')
 plt.xticks(rotation=45)
 plt.tight_layout()
 
 # Mostrar la gráfica en Streamlit
 st.pyplot(fig)
+
+# Descripción del proyecto
+st.markdown("""
+# Monitor de Mediciones
+Este proyecto muestra la evolución de las mediciones en diferentes períodos de tiempo. 
+Puedes seleccionar la columna de datos que deseas visualizar y el período de tiempo correspondiente.
+""")
