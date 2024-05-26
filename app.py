93d19b358776bf87f32c39cb3b1b90b1b3f7a18b
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2024-05-26 13:27:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
@@ -39,23 +40,17 @@ if opcion_visualizacion == 'Día actual':
     datos_visualizacion = datos_visualizacion.sort_values(by='Fecha')
     # Formatear la fecha para mostrar solo la hora
     datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M')
-    plt.plot(datos_visualizacion['Hora'], datos_visualizacion[columna_seleccionada], color='#33FFD1', marker='o')  # Modificación visual: Color personalizado y marcador
-    plt.xlabel('Hora')  # Modificación visual: Etiqueta del eje X
+    sns.lineplot(x='Hora', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    ax.set_xlabel('Hora')
     # Ajustar las etiquetas del eje X para mostrar solo cada hora
     x_ticks = datos_visualizacion['Hora'].iloc[::12]  # Cada 12*5 = 60 minutos = 1 hora
-    plt.xticks(rotation=45, ha='right')  # Modificación visual: Rotación y alineación de las etiquetas del eje X
-    plt.gca().set_xticks(x_ticks)  # Modificación visual: Establecer los ticks en el eje X
+    ax.set_xticks(x_ticks)
 else:
-    plt.plot(datos_visualizacion['Fecha'], datos_visualizacion[columna_seleccionada], color='#33FFD1', marker='o')  # Modificación visual: Color personalizado y marcador
-    plt.xlabel('Fecha')  # Modificación visual: Etiqueta del eje X
+    sns.lineplot(x='Fecha', y=columna_seleccionada, data=datos_visualizacion, ax=ax)
+    ax.set_xlabel('Fecha')
 
-plt.ylabel(columna_seleccionada)  # Modificación visual: Etiqueta del eje Y
-plt.title(f'{columna_seleccionada} a lo largo de {opcion_visualizacion}', color='#33FFD1', fontsize=16, fontweight='bold')  # Modificación visual: Título personalizado
-plt.yticks(color='#33FFD1')  # Modificación visual: Color de las etiquetas del eje Y
-plt.gca().spines['top'].set_visible(False)  # Modificación visual: Ocultar borde superior del gráfico
-plt.gca().spines['right'].set_visible(False)  # Modificación visual: Ocultar borde derecho del gráfico
-plt.gca().tick_params(axis='both', colors='#33FFD1')  # Modificación visual: Color de los ticks en ambos ejes
-
+ax.set_ylabel(columna_seleccionada)
+plt.xticks(rotation=45)
 plt.tight_layout()
 
 # Mostrar la gráfica en Streamlit
